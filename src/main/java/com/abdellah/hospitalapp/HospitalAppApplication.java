5a082b2f9b4bd043b7ef262262beb928a1aa6923
ABDELLAH MOUTAWAKKIL <abdo.moutawakkil2002@gmail.com>
2025-03-29 03:18:02 +0000

    Adding a Pagination and moving from the h2 db To MySQL

--- src/main/java/com/abdellah/hospitalapp/HospitalAppApplication.java
+++ src/main/java/com/abdellah/hospitalapp/HospitalAppApplication.java
@@ -29,35 +29,35 @@ public class HospitalAppApplication {
     //The bean annotation let us when we run the application all the methods
     // that has the bean annotation will be instantiated automatically  at first
 
-    @Bean
-    public CommandLineRunner start(){
-            return args -> {
-
-                // The first way how to add a Patient using the @NoArgsConstructor
-
-                Patient p1 = new Patient();
-                p1.setFirstName("Abdellah");
-                p1.setLastName("Moutawakkil");
-                p1.setDateOfBirth(new Date(2002,11,30));
-                p1.setScore(7);
-                p1.setSick(false);
-
-                patientRepository.save(p1);
-
-                // The second way how to add a Patient using the @AllArgsConstructor
-
-                Patient p2 = new Patient(null,"reda","kriate",new Date(2002,10,16),8,true);
-                patientRepository.save(p2);
-
-                // The third way using the @Builder lombok annotation
-
-                Patient p3 = Patient.builder().firstName("hamoushi").lastName("nessab").dateOfBirth(new Date(2002,11,17)).score(5).build();
-
-                patientRepository.save(p3);
-
-                List<Patient> patients = patientRepository.findAll();
-                // Displaying all the patients
-                patients.forEach(p -> System.out.println(p.toString()));
-            };
-    }
+//    @Bean
+//    public CommandLineRunner start(){
+//            return args -> {
+//
+//                // The first way how to add a Patient using the @NoArgsConstructor
+//
+//                Patient p1 = new Patient();
+//                p1.setFirstName("Abdellah");
+//                p1.setLastName("Moutawakkil");
+//                p1.setDateOfBirth(new Date(2002,11,30));
+//                p1.setScore(7);
+//                p1.setSick(false);
+//
+//                patientRepository.save(p1);
+//
+//                // The second way how to add a Patient using the @AllArgsConstructor
+//
+//                Patient p2 = new Patient(null,"reda","kriate",new Date(2002,10,16),8,true);
+//                patientRepository.save(p2);
+//
+//                // The third way using the @Builder lombok annotation
+//
+//                Patient p3 = Patient.builder().firstName("hamoushi").lastName("nessab").dateOfBirth(new Date(2002,11,17)).score(5).build();
+//
+//                patientRepository.save(p3);
+//
+//                List<Patient> patients = patientRepository.findAll();
+//                // Displaying all the patients
+//                patients.forEach(p -> System.out.println(p.toString()));
+//            };
+//    }
 }
--- src/main/java/com/abdellah/hospitalapp/web/PatientController.java
+++ src/main/java/com/abdellah/hospitalapp/web/PatientController.java
@@ -4,12 +4,15 @@ package com.abdellah.hospitalapp.web;
 import com.abdellah.hospitalapp.entities.Patient;
 import com.abdellah.hospitalapp.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+
 
 @Controller
 public class PatientController {
@@ -23,9 +26,14 @@ public class PatientController {
 
 
     @GetMapping("/AllPatients")
-    public String allPatients(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String allPatients(Model model
+            ,@RequestParam(name = "page",defaultValue = "0") int page
+            ,@RequestParam(name = "size",defaultValue = "5")int size)
+    {
+        Page<Patient> patients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("patients", patients.getContent());
+        model.addAttribute("totalPages", new int[patients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "allPatients";
     }
 
